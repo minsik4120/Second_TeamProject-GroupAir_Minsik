26794395e0997f9649a202937a376594281cba93
qkrwnsdn981204 <qkrwnsdn981204@gmail.com>
2024-05-14 12:32:59 +0900

    채commuting

--- src/main/java/org/spring/groupAir/commute/controller/CommuteController.java
+++ src/main/java/org/spring/groupAir/commute/controller/CommuteController.java
@@ -1,6 +1,8 @@
 package org.spring.groupAir.commute.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.spring.groupAir.commute.service.CommuteService;
+import org.spring.groupAir.department.entity.DepartmentEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +12,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/commute")
 public class CommuteController {
 
+    private final CommuteService commuteService;
+
+
     @GetMapping("/index")
     public String index(){
 
         return "commute/index";
     }
+    @GetMapping("/work")
+    public String work(){
+
+        return "commute/work";
+    }
+
+    @GetMapping("/vacation")
+    public String vacation(){
+
+        return "commute/vacation";
+    }
+
 
 }

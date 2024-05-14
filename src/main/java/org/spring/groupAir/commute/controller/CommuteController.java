d3b7675d4cfc47bd4bf8b4ea99011eebcc80a8a3
qkrwnsdn981204 <qkrwnsdn981204@gmail.com>
2024-05-14 14:20:53 +0900

    memberList

--- src/main/java/org/spring/groupAir/commute/controller/CommuteController.java
+++ src/main/java/org/spring/groupAir/commute/controller/CommuteController.java
@@ -3,9 +3,19 @@ package org.spring.groupAir.commute.controller;
 import lombok.RequiredArgsConstructor;
 import org.spring.groupAir.commute.service.CommuteService;
 import org.spring.groupAir.department.entity.DepartmentEntity;
+import org.spring.groupAir.member.dto.MemberDto;
+import org.spring.groupAir.member.service.MemberService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -13,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CommuteController {
 
     private final CommuteService commuteService;
+    private final MemberService memberService;
 
 
     @GetMapping("/index")
@@ -21,7 +32,26 @@ public class CommuteController {
         return "commute/index";
     }
     @GetMapping("/work")
-    public String work(){
+    public String work(@PageableDefault(page = 0, size = 5, sort = "id", direction = Sort.Direction.DESC) Pageable pageable,
+                       Model model,
+                       @RequestParam(name = "subject", required = false) String subject,
+                       @RequestParam(name = "search", required = false) String search){
+        Page<MemberDto> memberDtoPage = memberService.memberList(pageable, subject, search);
+
+        int totalPage = memberDtoPage.getTotalPages();//전체page
+        int newPage = memberDtoPage.getNumber();//현재page
+        Long totalElements = memberDtoPage.getTotalElements();//전체 레코드 갯수
+        int size = memberDtoPage.getSize();//페이지당 보이는 갯수
+
+        int blockNum = 3; //브라우저에 보이는 페이지 갯수
+
+        int startPage = (int) ((Math.floor(newPage / blockNum) * blockNum) + 1 <= totalPage
+            ? (Math.floor(newPage / blockNum) * blockNum) + 1 : totalPage);
+        int endPage = (startPage + blockNum) - 1 < totalPage ? (startPage + blockNum) - 1 : totalPage;
+
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
+        model.addAttribute("memberDtoPage", memberDtoPage);
 
         return "commute/work";
     }

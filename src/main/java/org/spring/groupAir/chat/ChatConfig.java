abc734d28bb20a868c210671d7c125dc263d1ff4
jomingeun <dgy6k@naver.com>
2024-05-23 17:26:57 +0900

    webSocket chat

--- src/main/java/org/spring/groupAir/chat/ChatConfig.java
+++ src/main/java/org/spring/groupAir/chat/ChatConfig.java
@@ -1,57 +1,52 @@
-package org.spring.groupAir.chat;
 
 import org.springframework.stereotype.Service;
 
 import javax.websocket.*;
 import javax.websocket.server.ServerEndpoint;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 @Service
 @ServerEndpoint(value = "/chat")
-public class ChatConfig {
+public class WebSocketConfigClass {
+    // 클라이언트 정보나 메세지를 set 설정
+    // 채팅창에 메세지를 반환
+    // 클라이언트 세션 정보를 가지고 있다, 접속(1), 해제 삭제
+    private static Set<Session> clientInfo =
+            Collections.synchronizedSet(new HashSet<Session>()); // 세션을 적절히 관리
 
-    private static Set<Session> employeeInfo =
-            Collections.synchronizedSet(new HashSet<Session>());
-
-    @OnOpen
-    private void onOpen(Session session) {
-        System.out.println("세션 시작: " + session.toString());
-
-        // 세션 정보가 없다면
-        if (!employeeInfo.contains(session)) {
-
+    @OnOpen // 접속 시 // 접속자 세션 설정
+    public void onOpen(Session session) {
+        System.out.println(" Session Start : " + session.toString());
+        if (!clientInfo.contains(session)) {
+            // 저장되어 있는 세션 정보가 없다면
             // 세션 설정
-            employeeInfo.add(session);
-
-            System.out.println("세션 열음: " + session);
+            clientInfo.add(session);
+            System.out.println(" Session open : " + session);
         } else {
-            System.out.println("세션 없음!");
+            System.out.println(" Pre Session ! ");
         }
     }
 
-    @OnMessage
+    @OnMessage // 클라이언트 메세지를 수신할 때 // 접속자 session 에 대한 메시지
     public void onMessage(String message, Session session) throws Exception {
 
-        System.out.println("메세지 정보: " + message);
-        for (Session sMessage : employeeInfo) {
-            System.out.println("받아온 메세지: " + sMessage);
-
-            // 전송자 URL
-            sMessage.getBasicRemote().sendText(message);
+        System.out.println(" Receive Message : " + message);
+        for (Session session1 : clientInfo) {
+            System.out.println(" Trans Data : " + message);
+            // .getBasicRemote() -> 전송자 URL
+            session1.getBasicRemote().sendText(message);
         }
     }
 
-    @OnClose
+    @OnClose // 접속 해제시 // 접속자 : 해제 설정
     public void onClose(Session session) {
-        System.out.println("세션 종료: " + session);
-        employeeInfo.remove(session); // 세션 삭제
+        System.out.println(" Session Close : " + session);
+        clientInfo.remove(session); // 세션 삭제
     }
 
-    @OnError
-    public void error(Throwable throwable) {
-        System.out.println("채팅 에러!!!: ");
+    @OnError // 에러 발생 시
+    public void handleError(Throwable throwable) {
+        System.out.println(" WebSocket Error ! : ");
         throwable.printStackTrace();
     }
 

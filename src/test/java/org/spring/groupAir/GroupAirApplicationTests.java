1879421862599927426d67f3634f829f4749f1ac
jomingeun <dgy6k@naver.com>
2024-05-27 09:48:51 +0900

    queryDsl

--- src/test/java/org/spring/groupAir/GroupAirApplicationTests.java
+++ src/test/java/org/spring/groupAir/GroupAirApplicationTests.java
@@ -1,13 +1,33 @@
 package org.spring.groupAir;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.Test;
+import org.spring.groupAir.member.entity.MemberEntity;
+import org.spring.groupAir.member.entity.QMemberEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @SpringBootTest
+@Configuration
 class GroupAirApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private JPAQueryFactory queryFactory;
+
+    @Test
+    public void test() {
+
+        QMemberEntity member = QMemberEntity.memberEntity;
+
+        List<MemberEntity> memberEntityList = queryFactory.selectFrom(member).fetch();
+
+        System.out.println("member: >>>" + member);
+        System.out.println("memberEntityList: >>>" + memberEntityList);
+
+    }
+
 
 }
